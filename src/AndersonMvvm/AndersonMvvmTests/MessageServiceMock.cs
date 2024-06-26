d4c5b8fe4f4479785814f475a23adddc9eb15af9
wolvesdevs_workPC <75551104+wolvesdevs@users.noreply.github.com>
2024-06-26 09:15:48 +0900

    refs #015 18. Moqを使って簡単にテスト用Mockを作成する方法

--- src/AndersonMvvm/AndersonMvvmTests/MessageServiceMock.cs
+++ src/AndersonMvvm/AndersonMvvmTests/MessageServiceMock.cs
@@ -1,16 +1,16 @@
-﻿using AndersonMvvm.BindHelper;
-using System.Windows.Forms;
+﻿//using AndersonMvvm.BindHelper;
+//using System.Windows.Forms;
 
-namespace AndersonMvvmTests;
-internal sealed class MessageServiceMock : IMessageService
-{
-    public void OKOnly(string message)
-    {
-    }
+//namespace AndersonMvvmTests;
+//internal sealed class MessageServiceMock : IMessageService
+//{
+//    public void OKOnly(string message)
+//    {
+//    }
 
-    public DialogResult QuestionOKCancel(string message)
-    {
-        //return DialogResult.OK;
-        return DialogResult.Cancel;
-    }
-}
+//    public DialogResult QuestionOKCancel(string message)
+//    {
+//        //return DialogResult.OK;
+//        return DialogResult.Cancel;
+//    }
+//}
--- src/AndersonMvvm/AndersonMvvmTests/UnitTest1.cs
+++ src/AndersonMvvm/AndersonMvvmTests/UnitTest1.cs
@@ -1,4 +1,7 @@
+using AndersonMvvm.BindHelper;
 using AndersonMvvm.ViewModels;
+using Moq;
+using System.Windows.Forms;
 
 namespace AndersonMvvmTests;
 
@@ -7,7 +10,10 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
-        var vm = new MainViewModel(new MessageServiceMock());
+        var mock = new Mock<IMessageService>();
+        mock.Setup(x=>x.QuestionOKCancel("ï€ë∂ÇµÇ‹Ç∑Ç©ÅH")).Returns(DialogResult.Cancel);
+
+        var vm = new MainViewModel(mock.Object);
         vm.Save();
         Assert.Equal("SAVE!!", vm.AAALabelText);
     }
